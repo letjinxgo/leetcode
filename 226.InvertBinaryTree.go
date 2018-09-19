b6c7941e4fd4d13254c00888de1667bb551c9902
Nico <letjinxgo@gmail.com>
2018-09-19 11:02:05 +0800

    Invert Binary Tree

--- 226.InvertBinaryTree.go
+++ 226.InvertBinaryTree.go
@@ -1,6 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func main() {
+	nums := &TreeNode{4,
+		&TreeNode{7,
+			&TreeNode{9, nil, nil},
+			&TreeNode{6, nil, nil}},
+		&TreeNode{2,
+			&TreeNode{3, nil, nil},
+			&TreeNode{1, nil, nil}}}
+	/* 	numss := preOrderRecursive(nums)
+	 */
+	numsss := breadthFirstSearch(nums)
+	fmt.Println(numsss)
+}
 
 type TreeNode struct {
 	Val   int
@@ -8,14 +24,45 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func main() {
-	root := &TreeNode{1, &TreeNode{3, nil, nil}, &TreeNode{2, nil, nil}}
-	fmt.Println(invertTree(root))
-}
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
 func invertTree(root *TreeNode) *TreeNode {
 	if root == nil {
 		return nil
 	}
 	root = &TreeNode{root.Val, invertTree(root.Right), invertTree(root.Left)}
 	return root
+}
+
+/* func preOrderRecursive(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	var nums []int
+	nums = append(nums, preOrderRecursive(root.Left)...)
+	nums = append(nums, root.Val)
+	nums = append(nums, preOrderRecursive(root.Right)...)
+	return nums
+} */
+func breadthFirstSearch(node *TreeNode) []int {
+	var result []int
+	var nodes []*TreeNode = []*TreeNode{node}
+	for len(nodes) > 0 {
+		node = nodes[0]
+		nodes = nodes[1:]
+		result = append(result, node.Val)
+		if node.Left != nil {
+			nodes = append(nodes, node.Left)
+		}
+		if node.Right != nil {
+			nodes = append(nodes, node.Right)
+		}
+	}
+	return result
 }
